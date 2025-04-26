4d12afad1da9439457ebba02cfab528026e3964f
Kai <131925584+Kaisan5@users.noreply.github.com>
2025-04-27 01:49:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -137,7 +137,7 @@ async def start_command(client: Client, message: Message):
 
         if FILE_AUTO_DELETE > 0:
             notification_msg = await message.reply(
-                f"<b>This file will be deleted in <a href=https://t.me/Flix_Dex>{get_exp_time(FILE_AUTO_DELETE)} </a>.<b> Please save or forward it to your saved messages before it gets deleted.</b>\n\n<b>Helpline @EternalsHelplineBot</b>"
+                f"<b>This file will be deleted in <a href=https://t.me/Flix_Dex>{get_exp_time(FILE_AUTO_DELETE)}</a>Please save or forward it to your saved messages before it gets deleted\n\n<a>Helpline @EternalsHelplineBot</b>"
             )
 
             await asyncio.sleep(FILE_AUTO_DELETE)
